71604956b6e214040b041842e4b49c9e8b8f4afe
Alex Kochetov <kochetovalex@yahoo.com>
2023-02-07 00:05:37 +0100

    add main structure

--- src/main/java/com/example/springboottemplate/employee/controller/EmployeeController.java
+++ src/main/java/com/example/springboottemplate/employee/controller/EmployeeController.java
@@ -1,6 +1,5 @@
 package com.example.springboottemplate.employee.controller;
 
-
 import com.example.springboottemplate.employee.entity.Employee;
 import com.example.springboottemplate.employee.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,32 +12,22 @@ import java.util.List;
 @RestController
 @RequestMapping("/employee")
 public class EmployeeController {
-
-    @Autowired
     private EmployeeService employeeService;
+    @Autowired
+    public EmployeeController() {
+
+    }
+    public List<Employee>getEmployees() {
+        return null;
+    }
+    @GetMapping("/{empId}")
+    public Employee getEmployeeById(@PathVariable long empId) {
+        return employeeService.getEmployeeById(empId);
+    }
 
     @PostMapping("/save")
-    public ResponseEntity<Employee> saveEmployee(@RequestBody Employee empObj) {
-        employeeService.saveEmployee(empObj);
-        return new ResponseEntity<>(HttpStatus.CREATED);
-    }
-
-    @GetMapping(value = {"/getEmployees", "/{empId}"})
-    public List<Employee> getEmployee(@PathVariable(required = false) Long empId) {
-        return employeeService.getEmployee(empId);
-    }
-
-    @DeleteMapping("delete/{empId}")
-    public ResponseEntity removeEmployee(@PathVariable Long empId){
-        employeeService.deleteEmployee(empId);
-        return new ResponseEntity(HttpStatus.OK);
-    }
-
-    @PutMapping("/{empId}/project/{projectId}")
-    public Employee assignProjectToEmployee(
-            @PathVariable Long empId,
-            @PathVariable Long projectId
-    ){
-        return employeeService.assignProjectToEmployee(empId, projectId);
+    public ResponseEntity saveEmployee(@RequestBody Employee employee) {
+        employeeService.saveEmployee(employee);
+        return new ResponseEntity(HttpStatus.CREATED);
     }
 }
--- src/main/java/com/example/springboottemplate/employee/service/EmployeeService.java
+++ src/main/java/com/example/springboottemplate/employee/service/EmployeeService.java
@@ -1,48 +1,18 @@
 package com.example.springboottemplate.employee.service;
 
 import com.example.springboottemplate.employee.entity.Employee;
-import com.example.springboottemplate.employee.repository.EmployeeRepository;
-import com.example.springboottemplate.project.entity.Project;
-import com.example.springboottemplate.project.repository.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Set;
-
-@Service
 public class EmployeeService {
-    private EmployeeRepository employeeRepository;
-    private ProjectRepository projectRepository;
-
     @Autowired
-    public EmployeeService(EmployeeRepository employeeRepository, ProjectRepository projectRepository) {
-        this.employeeRepository = employeeRepository;
-        this.projectRepository = projectRepository;
-    }
-    public void saveEmployee(Employee empObj) {
-        employeeRepository.save(empObj);
+    public EmployeeService() {
+
     }
 
-    public List<Employee> getEmployee(Long empId) {
-        return employeeRepository.findAllByEmpId(empId);
+    public void saveEmployee(Employee employee) {
     }
 
-    public List<Employee> getAllEmployees() {
-        return employeeRepository.findAll();
-    }
-
-    public void deleteEmployee(Long empId) {
-        employeeRepository.deleteById(empId);
-    }
-
-    public Employee assignProjectToEmployee(Long empId, Long projectId) {
-        Set<Project> projectSet = null;
-        Employee employee = employeeRepository.findById(empId).get();
-        Project project = projectRepository.findById(projectId).get();
-        projectSet =  employee.getAssignedProjects();
-        projectSet.add(project);
-        employee.setAssignedProjects(projectSet);
-        return employeeRepository.save(employee);
+    public Employee getEmployeeById(long empId) {
+        return null;
     }
 }
